a3164fbcd234ffde3ce775a5f8ee81d1d1606d52
Madison Migliori <145717048+madisonmigliori@users.noreply.github.com>
2023-11-28 09:11:19 -0700

    perlas restore code

--- Reports/GUI.java
+++ Reports/GUI.java
@@ -1,17 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.*;
 import java.io.IOException;
-import java.util.Scanner;
-
 import javax.swing.*;
-import javax.swing.text.AttributeSet.FontAttribute;
 
 public class GUI implements ActionListener {
     static JFrame f;
 
-    public GUI() throws IOException {
+    public GUI() {
         // Requirement: 1 ) Import Application Data - The report Engine subsystem shall be able to import 
         //                  the application name, ID, year, Academic Standing, GPA, Major,Minor, Expected
         //                  graduation date, field of interest, gender, transfer student, units, and 
@@ -25,8 +19,7 @@ public class GUI implements ActionListener {
         ReadInputCsvFiles applicantsData = new ReadInputCsvFiles();
         DonorReport donorData = new DonorReport();
         AwardDisbursementReport disburmentData = new AwardDisbursementReport();
-        ScholarshipReports scholarshipReportsData = new ScholarshipReports();
-        MatchingReport matchingData = new MatchingReport();
+        ScholarshipReports ScholarshipReportsData = new ScholarshipReports();
 
                 
         // Additional arguments can be set to do different things.
@@ -39,24 +32,9 @@ public class GUI implements ActionListener {
         int[] recordAmount = applicantsData.parseCsvInfo(args);
         int numDonor = donorData.readFile();
         disburmentData.readIn("Awarded.csv");
-        matchingData.ReadCSVFile();
 
         // create a new frame
         f = new JFrame("Report Generator");
-
-        //Font
-        Font uaFont = new Font("Arial",Font.BOLD, 12);
-
-        //Icon
-        ImageIcon ua = new ImageIcon("ua_block_rgb_blue.png");
-        f.setIconImage(ua.getImage());
-
-        //Header
-        // JLabel headerLabel = new JLabel("University of Arizona Report Generator");
-        // headerLabel.setBackground(new Color(171, 5, 32));
-        // headerLabel.setForeground(new Color(171, 5, 32)); // Customize font color if needed
-        // headerLabel.setBounds(90, 10, 300, 30); // Adjust the bounds according to your layout
-        // f.add(headerLabel, BorderLayout.CENTER);
 
         // Create Button1. If pressed, Open Scholarships GUI
         JButton button1 = new JButton( new AbstractAction("Scholarships") {
@@ -65,11 +43,7 @@ public class GUI implements ActionListener {
                 new ScholarshipGUI(applicantsData.scholarship, recordAmount, applicantsData.student, applicantsData.awarded);
             }
         });
-
         button1.setBounds(90, 50, 200, 40);
-        // // button1.setBackground(new Color(171, 5, 32));
-        // // button1.setFont(uaFont);
-        // button1.setForeground(Color.WHITE);
 
          // Create Button2. If pressed, Open Applicants Data GUI
         JButton button2 = new JButton(new AbstractAction("Applicants Report"){
@@ -84,10 +58,7 @@ public class GUI implements ActionListener {
                 }
             }
         });
-        button1.setBounds(90, 100, 200, 40);;
-        // button2.setBackground(new Color(171, 5, 32));
-        // button2.setFont(uaFont);
-        // button2.setForeground(Color.WHITE);
+        button2.setBounds(90, 100, 200, 40);
         
         // Create Button3. If pressed, Open Donor Data GUI
         JButton button3 = new JButton(new AbstractAction("Donors Report"){
@@ -102,37 +73,9 @@ public class GUI implements ActionListener {
             }
         });
         button3.setBounds(90, 150, 200, 40);
-        // button3.setBackground(new Color(171, 5, 32));
-        // button3.setFont(uaFont);
-        // button3.setForeground(Color.WHITE);
 
-        JButton button4 = new JButton(new AbstractAction("Matching Report"){
-            @Override
-            public void actionPerformed( ActionEvent e ) {
-                ///new DonorGUI(applicantsData.scholarship, recordAmount);
-                    try {
-                        matchingData.GenerateReport(matchingData.getDataList());
-                       // matchingData.CustomizedReport(matchingData.getDataList());
-       
-        // Process the input
-   
-                        
-                    } catch (IOException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-                
-            }
-        });
-
-        button4.setBounds(90, 200, 200, 40);
-        // button4.setBackground(new Color(171, 5, 32));
-        // button4.setFont(uaFont);
-        // button4.setForeground(Color.WHITE);
-
-
-        // Create Button. If pressed, Open Donor Data GUI
-        JButton button5 = new JButton(new AbstractAction("Disbursement Report"){
+        // Create Button4. If pressed, Open Donor Data GUI
+        JButton button4 = new JButton(new AbstractAction("Disbursement Report"){
             @Override
             public void actionPerformed( ActionEvent e ) {
                 try {
@@ -144,65 +87,47 @@ public class GUI implements ActionListener {
                 }
             }
         });
-        button5.setBounds(90, 250, 200, 40);
-        // button5.setBackground(new Color(171, 5, 32));
-        // button5.setFont(uaFont);
-        // button5.setForeground(Color.WHITE);
-
-        //Create Button
+        button4.setBounds(90, 200, 200, 40);
 
         // Create Button5. If pressed, Sue's code will be launch
-        JButton button6 = new JButton(new AbstractAction("Additional Scholarship Filters"){
+        JButton button5 = new JButton(new AbstractAction("Additional Scholarship Filters"){
             @Override
             public void actionPerformed( ActionEvent e ) {
                 ScholarshipReportsData.ScholarReport(applicantsData.awarded);
             }
         });
-        button6.setBounds(90, 300, 200, 40);
-        // button6.setBackground(new Color(171, 5, 32));
-        // button6.setFont(uaFont);
-        // button6.setForeground(Color.WHITE);
+        button5.setBounds(90, 250, 200, 40);
 
         // Create Button6. If pressed, Sue's code will be launch
-        JButton button7 = new JButton(new AbstractAction("Yearly or Monthly Report"){
+        JButton button6 = new JButton(new AbstractAction("Yearly or Monthly Report"){
             @Override
             public void actionPerformed( ActionEvent e ) {
                 new YearlyMonthlyGUI(applicantsData.scholarship, recordAmount, applicantsData.awarded);
             }
         });
 
-        button7.setBounds(90, 350, 200, 40);
-        // button7.setBackground(new Color(171, 5, 32));
-        // button7.setFont(uaFont);
-        // button7.setForeground(Color.WHITE);
+        button6.setBounds(90, 300, 200, 40);
 
         // Create Button7. Closes GUI
-        JButton button8 = new JButton(new AbstractAction("Exit"){
+        JButton button7 = new JButton(new AbstractAction("Exit"){
             @Override
             public void actionPerformed( ActionEvent e ) {
                 System.exit(0);
             }
         });
-        button8.setBounds(250, 400, 100, 50);
-        // button8.setBackground(new Color(171, 5, 32));
-        // button8.setFont(uaFont);
-        // button8.setForeground(Color.WHITE);
+        button7.setBounds(250, 350, 100, 50);
 
 
-        f.add(button1, BorderLayout.CENTER);
-        f.add(button2, BorderLayout.CENTER);
-        f.add(button3, BorderLayout.CENTER);
-        f.add(button4, BorderLayout.CENTER);
-        f.add(button5, BorderLayout.CENTER);
-        f.add(button6, BorderLayout.CENTER);
-        f.add(button7, BorderLayout.CENTER);
-        f.add(button8, BorderLayout.CENTER);
-
-        
+        f.add(button1);
+        f.add(button2);
+        f.add(button3);
+        f.add(button4);
+        f.add(button5);
+        f.add(button6);
+        f.add(button7);
 
         // set the size of frame
-        f.setSize(500, 500); 
-       // f.getContentPane().setBackground(new Color(244, 237, 229)) ; 
+        f.setSize(450, 450);   
         f.setLayout(null);
         f.setVisible(true);
     }
